b7bb4f6eb64c9000c4bd2f5324bc8afcdf67e957
Naiman <85957227+NaimanMpl@users.noreply.github.com>
2024-06-23 23:41:28 +0200

    feat: added admin protected endpoints

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,25 @@
-import { PrismaClient } from '@prisma/client/edge';
-import jwt from 'jsonwebtoken';
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import { isAuthenticated } from './app/lib/auth';
+
+const PROTECTED_ADMIN_ROUTES = [
+  '/api/users',
+  '/api/users/add',
+  '/api/users/update',
+  '/api/roles',
+]
 
 export async function middleware(request: NextRequest) {
 
   const clientCookies = cookies();
   const userPayload = await isAuthenticated(request, clientCookies);
+
+  if (PROTECTED_ADMIN_ROUTES.includes(request.nextUrl.pathname)) {
+    if (!userPayload || !userPayload.admin) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 403 });
+    }
+  }
+
   if (request.nextUrl.pathname.startsWith('/login')) {
     if (userPayload !== null) {
       return NextResponse.redirect(new URL('/', request.url));
